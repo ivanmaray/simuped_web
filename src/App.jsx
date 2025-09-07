bd0892ee5f7cf1cb619a4083cb0042d075fad56a
Iván Maray Mateos <ivanmaray@Ordenador-portatil-de-Ivan.local>
2025-09-07 14:44:08 +0200

    7sept

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ useEffect(() => {
             src="/logo-negative.png"
             alt=""
             aria-hidden
-            className="hidden sm:block absolute left-0 -top-10 h-12 w-auto opacity-95 select-none pointer-events-none z-0"
+            className="hidden sm:block absolute left-12 top-1/4 h-48 w-auto opacity-20 select-none pointer-events-none z-0"
             style={{ filter: 'none' }}
           />
           {/* Decorative background accents */}
