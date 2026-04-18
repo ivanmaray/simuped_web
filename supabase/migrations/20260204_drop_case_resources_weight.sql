2de8d3abf87b1fc811db5b32c607446c6acdde73
Iván Maray <ivanmaraymateos@gmail.com>
2026-04-18 18:57:43 +0200

    mejoras

--- supabase/migrations/20260204_drop_case_resources_weight.sql
+++
@@ -1,9 +0,0 @@
--- Drop obsolete relevance column from case resources
--- Run in Supabase SQL editor or migration pipeline
-
-BEGIN;
-
-ALTER TABLE public.case_resources
-  DROP COLUMN IF EXISTS weight;
-
-COMMIT;
